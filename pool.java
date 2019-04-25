436e3e43b77daf441c6d3eeb28d5ea8770635150
minhalaliawan <48066167+minhalaliawan@users.noreply.github.com>
2019-04-26 01:05:28 +0500

    Add files via upload

---
+++ pool.java
@@ -0,0 +1,24 @@
+classPoolPuzleOne{
+   public static void main(string [] args) {
+        int x=0;
+        while (x<4){
+         System.out.print("a");
+         if(x<1){
+            System.out.print(" ");
+          }
+         System.out.print("n");
+         if(x>1){
+            System.out.print("oyster");
+            x=x+2;
+          }
+          if(x==1){
+            System.out.print("noys");
+          }
+          if(x<1){
+            System.out.print("oise");
+          }
+          System.out.println("");
+          X=X+1;
+       }
+      }
+    }
